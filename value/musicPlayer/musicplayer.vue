b618134049d1f1b34f3b955df35cb084cfba78e2
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-25 22:09:00 +0200

    Update musicplayer.vue

--- value/musicPlayer/musicplayer.vue
+++ value/musicPlayer/musicplayer.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="app">
-
-
+    <header>
+      <h1>Vue 3 Music Player</h1>
+      <input type="file" @change="handleFiles" multiple webkitdirectory directory />
+    </header>
+    <main>
+      <ul>
+        <li v-for="(file, index) in audioFiles" :key="index" @click="playAudio(file)">
+          {{ file.name }}
+        </li>
+      </ul>
+      <audio ref="audioPlayer" controls></audio>
+    </main>
 </template>
